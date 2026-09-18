bc672b29668061a87d5bc59c2fbd2ed8b14e1cb5
charlesguernon2008@gmail.com <charlesguernon2008@gmail.com>
2026-09-18 14:55:37 -0400

    Labo en cours

--- Laboratoires/Laboratoire01/obj/Release/net10.0-windows/Laboratoire01.AssemblyInfo.cs
+++ Laboratoires/Laboratoire01/obj/Release/net10.0-windows/Laboratoire01.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Laboratoire01")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Release")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+28e685eca4b50bcf14e02d614322de0755debb08")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+431617d1308d0c6b91e633d570a4df6c73c1ee39")]
 [assembly: System.Reflection.AssemblyProductAttribute("Laboratoire01")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Laboratoire01")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
